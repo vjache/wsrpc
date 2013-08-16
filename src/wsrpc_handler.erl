dc2ea00afe3841bb56e689313fc99a86af61a1c0
Vyacheslav Vorobyov <vjache@gmail.com>
2013-08-16 21:06:56 +0400

    Add  feature to make calls through HTTP/JSON via POST method.

--- src/wsrpc_handler.erl
+++ src/wsrpc_handler.erl
@@ -6,17 +6,60 @@
 
 -include("log.hrl").
 
--export([init/3, 
-	 handle/2, 
-	 terminate/3
-]).
+-export([ init/3, 
+	  handle/2, 
+	  terminate/3 ]).
 
--export([websocket_init/3, 
-	 websocket_handle/3,
-	 websocket_info/3, 
-	 websocket_terminate/3]).
+-export([ websocket_init/3, 
+	  websocket_handle/3,
+	  websocket_info/3, 
+	  websocket_terminate/3 ]).
 
--export([stream/2]).
+-export([ stream/2 ]).
+
+%%////////////////////////////////////////////////////////////////////////////////
+-record(http_req, {
+        %% Transport.
+        socket = undefined :: undefined | inet:socket(),
+        transport = undefined :: undefined | module(),
+        connection = keepalive :: keepalive | close,
+
+        %% Request.
+        pid = undefined :: pid(),
+        method = <<"GET">> :: binary(),
+        version,
+        peer = undefined :: undefined | {inet:ip_address(), inet:port_number()},
+        host = undefined :: undefined | binary(),
+        host_info,
+        port,
+        path = undefined :: binary(),
+        path_info,
+        qs = undefined :: binary(),
+        qs_vals = undefined :: undefined | list({binary(), binary() | true}),
+        fragment = undefined :: binary(),
+        bindings,
+        headers,
+        p_headers = [] :: [any()], %% @todo Improve those specs.
+        cookies = undefined :: undefined | [{binary(), binary()}],
+        meta = [] :: [{atom(), any()}],
+
+        %% Request body.
+        body_state = waiting :: waiting | done
+                | {stream, non_neg_integer(), fun(), any(), fun()},
+        multipart = undefined :: undefined | {non_neg_integer(), fun()},
+        buffer = <<>> :: binary(),
+
+        %% Response.
+        resp_compress = false :: boolean(),
+        resp_state = waiting :: locked | waiting | chunks | done,
+        resp_headers,
+        resp_body,
+
+        %% Functions.
+        onresponse
+}).
+
+%%////////////////////////////////////////////////////////////////////////////////
 
 %% WebSocket Handler state
 -record(state, {service :: {gen_server, pid()} | 
@@ -57,18 +100,53 @@ init({_Any, http}, Req, Opts) ->
 			  {upgrade, protocol, cowboy_websocket} 
 		  end,
     case cowboy_req:header(<<"upgrade">>, Req) of
-	{undefined, Req2} ->
-	    ?LOG_DEBUG([ordinary_http_req]),
-	    {ok, Req2, undefined};
+	{undefined, _Req2} ->
+	    rest_init(Req, Opts);
+	    %% ?LOG_DEBUG([{rest_upgrade, Req}]),
+	    %% {upgrade, protocol, cowboy_rest};
 	{<<"websocket">>, _Req2} ->
 	    DoWsUpgrade();
 	{<<"WebSocket">>, _Req2} ->
 	    DoWsUpgrade()
     end.
 
-handle(Req, State) ->
+handle(Req, #state{ types_cache = TyCache, 
+		    service     = {gen_server, Pid}} = State) ->
     ?LOG_DEBUG([{http_handle, Req}]),
-    {ok, Req, State}.
+    %% 1. Get JSON request body
+    case cowboy_req:method(Req) of
+	{<<"POST">>, Req1} -> 
+	    #http_req{buffer = ReqJson} = Req,
+	    ?LOG_DEBUG([{msg_rcv, ReqJson}]),
+	    {RespJson, State1} = case jsx:decode(ReqJson) of
+				     {incomplete, _} ->
+					 { <<"{\"type\":\"error\", \"reason\":\"incomplete_json\"}">>, State};
+				     ReqJsx ->
+					 ?LOG_DEBUG([{msg_jsxed, ReqJsx}]),
+					 try jsx_util:from_jsx(
+					       ReqJsx, TyCache, 
+					       fun(Type)-> get_type(State, Type) end) of
+					     {Call, TyCache1} ->
+						 ?LOG_DEBUG([{jsx_mapped_to_record, Call}]),
+						 Resp = gen_server:call( Pid, Call, infinity),
+						 ?LOG_DEBUG([service_called_succesfully, {call, Call}, {pid, Pid} ]),
+						 make_reply(Resp, State#state{ types_cache = TyCache1})
+					 catch
+					     _:Reason ->
+						 ?LOG_ERROR([ jsx_to_record_failed, 
+							      {reason, Reason} ]),
+						 { <<"{\"type\":\"error\", \"reason\":\"json_to_record_failed\"}">>, State}
+					 end
+				 end,
+	    cowboy_req:reply( 200,
+			      [{<<"content-encoding">>, <<"utf-8">>}, 
+			       {<<"content-type">>,     <<"application/json">>}], 
+			      RespJson, Req1);
+ 	{_, Req1} -> 
+	    State1 = State,
+ 	    cowboy_req:reply(405, Req)
+    end,
+    {ok, Req1, State1}.
 
 terminate(Reason, _Req, _State) ->
     ?LOG_DEBUG([http_terminate, 
@@ -76,26 +154,41 @@ terminate(Reason, _Req, _State) ->
 	       {state, _State}]),
      ok.
 
-%%
-%% Websocket paragraph
-%%
+%%------------------------------------------------------------------------
+%% REST paragraph
+%%------------------------------------------------------------------------
+rest_init(Req, Opts) ->
+    Service = resolve_service(Req, Opts),
+    TyCache = dict:new(),
+    {ok,
+     cowboy_req:compact(Req),
+     #state{service     = Service,
+	    types_cache = TyCache }}.
 
-websocket_init(_Any, Req, Opts) ->
+%%------------------------------------------------------------------------
+%% Websocket paragraph
+%%------------------------------------------------------------------------
+
+resolve_service(Req, Opts) ->
     {ServicePath, _} = cowboy_req:path_info(Req),
     RMod    = proplists:get_value(resolver, Opts, wsrpc_gs_resolver),
     Service = RMod:resolve(ServicePath),
+    ?LOG_DEBUG([{service_resolved, ServicePath}, {options, Opts}]),
+    Service.
+
+websocket_init(_Any, Req, Opts) ->
+    Service = resolve_service(Req, Opts),
     case Service of
 	{gen_server, Pid} -> erlang:monitor(process, Pid);
 	_ -> ok
     end,
-    ?LOG_DEBUG([{ws_init, ServicePath, Opts}]),
     TyCache = dict:from_list(
 		[?t('call'),
 		 ?t('stream-item'),
 		 ?t('stream-end')]),
-    {ok, 
-     cowboy_req:compact(Req), 
-     #state{service     = Service, 
+    {ok,
+     cowboy_req:compact(Req),
+     #state{service     = Service,
 	    types_cache = TyCache },
      hibernate}.
 
@@ -174,12 +267,15 @@ websocket_terminate(TermMsg, _Req, _State) ->
 get_type(#state{service = {gen_server, Pid} }, Type) ->
     gen_server:call(Pid, {get_type, Type}).
 
-make_reply(ReplyObj, Req, #state{types_cache = TyCache} = State) ->
+make_reply(ReplyObj, Req, State) ->
+    {Json, State1} = make_reply(ReplyObj, State),
+    {reply, {text, Json }, Req, State1 }.
+
+make_reply(ReplyObj, #state{types_cache = TyCache} = State) ->
     {Jsx, TyCache1} = jsx_util:to_jsx(
 			ReplyObj, TyCache, 
 			fun(Type)-> get_type(State, Type) end),
-    {reply, {text, jsx:encode(Jsx)}, 
-     Req, State#state{types_cache = TyCache1} }.
+    {jsx:encode(Jsx), State#state{types_cache = TyCache1} }.
 
 lists_keyupsert(_Key, _N, [], Fun) ->
     case Fun(undefined) of
